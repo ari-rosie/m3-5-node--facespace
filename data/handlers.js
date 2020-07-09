22d6a6d1c0cec54f1a3b3dc9497f1450a083deff
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-09 13:45:08 -0400

    Exercice-4.2

--- data/handlers.js
+++ data/handlers.js
@@ -1,5 +1,8 @@
 const { users } = require('./users');
 const func = require('./handlers-utilities');
+const { getUserById } = require('./handlers-utilities');
+let signFlag = false;
+let loggedUserId = 0;
 
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
@@ -8,31 +11,36 @@ const handleFourOhFour = (req, res) => {
 
 //homepage function
 const homepage = (req, res) => {
-    res.status(200).render('pages/homepage', {users: users});
+    res.status(200).render('pages/homepage', {loggedUser: getUserById(loggedUserId, users), users: users, logged: signFlag});
 };
 
 //profile page function
 const profilePage = (req, res) => {
     const user = func.getUserById(req.params.id, users);
     const friends = func.getFriends(user.friends, users);
-    console.log(friends);
-    res.status(200).render('pages/profile', {user: user, friends: friends});
-}
+    res.status(200).render('pages/profile', {loggedUser: getUserById(loggedUserId, users), user: user, friends: friends, logged: signFlag});
+};
 
 //sign in page function
 const signIn = (req, res) => {
-    res.status(200).render('pages/signin');
-}
+    if (!signFlag)
+        res.status(200).render('pages/signin', {loggedUser: getUserById(loggedUserId, users), logged: signFlag});
+    else 
+        res.status(200).redirect(`/users/${loggedUserId}`);
+};
 
 //submit name function
 const submitName = (req, res) => {
     const firstName = req.body.firstName.toLowerCase();
     const user = func.getUserByName(firstName, users);
-    if (user)
+    if (user){
+        loggedUserId = user._id;
+        signFlag = true;
         res.status(200).redirect(`/users/${user._id}`);
+    }
     else
         res.status(404).redirect('/*');
-}
+};
 
 module.exports = { handleFourOhFour, homepage, profilePage, signIn, submitName };
 
